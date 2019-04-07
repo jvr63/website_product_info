9b7b5d6e00ade50c95cbbb34d55494ce71453d11
Juan Vázquez Reina <juan@guadalsystem.ddns.net>
2019-04-07 18:28:53 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Show advanced product info",
+    'summary': 'Show template info into product',
+    'description': """
+Show more info into template website_sale.product as EAN/REF, etc
+    """,
+    'author': 'Juan Vázquez Moreno',
+    'category': 'Website',
+    'version': '1.0',
+    'depends': ['website_sale'],
+    'data': [
+        'views/website_sale_template_info.xml'
+    ],
+    'installable': True,
+}
