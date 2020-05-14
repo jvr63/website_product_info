87fc76a1abf78782fd2be20f7db214d746c0325a
JVR <jvr@guadalsistema.com>
2020-05-14 14:50:07 +0200

    update

---
+++ website_product_info/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Show advanced product info",
+    'summary': 'Show more product info',
+    'description': """
+Show more info into template website_sale.product as EAN/REF, etc
+    """,
+    'author': 'Juan Vázquez Moreno y JVR',
+    'category': 'Website',
+    'version': '1.0',
+    'depends': ['website_search'],
+    'data': [
+        'views/website_sale_template_info.xml',
+        'views/website_sale_product_info.xml',
+        'views/website_sale_template_produts_description.xml',
+    ],
+    'installable': True,
+}
